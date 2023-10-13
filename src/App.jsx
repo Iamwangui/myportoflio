ebfa34b9f575df3f99eeb9b1b0d02c01c9ee2e02
Iamwangui <99409074+Iamwangui@users.noreply.github.com>
2023-10-13 12:24:21 +0000

    Added personal information

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Grace Wangui",
+  title: "Aspiring Data Scientist",
+  email: "gracegitau902@gmail.com",
+  gitHub: "https://github.com/Iamwangui",
+  instagram: "",
+  linkedIn: "www.linkedin.com/in/grace-wangui-197171219",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "",
+  youTube: "",
 };
 
 const primaryColor = "#4E567E";
